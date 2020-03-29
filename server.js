edc81d3e6e695f44bd37f630853827d498836edc
Vitor Isaac <vitor.maia@hammerconsult.com.br>
2020-03-29 16:19:50 -0300

    tweet com hora e minutos

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const twitterClient = require('./twitter.js');
 const request = require('request');
-const moment = require('moment');
+const moment = require('moment-timezone');
 const CronJob = require('cron').CronJob;
 
 
-
+const timezone = 'America/Sao_Paulo';
 const ApiUrl = 'https://coronavirus-19-api.herokuapp.com'
 const port = process.env.YOUR_PORT || process.env.PORT || 3000;
 const host = process.env.YOUR_HOST || '0.0.0.0';
@@ -19,32 +19,25 @@ app.listen(port, host, function () {
 
 tweetDataWorld = () => {
     request.get(`${ApiUrl}/all`, {json: true} ,(error, res, body) => {
-        tweet = `${moment().format('DD/MM/YY').toString()}, COVID-19 no mundo. \n\nCasos confirmados: ${body.cases} \n\nRecuperados: ${body.recovered} \n\nMortes: ${body.deaths}`
-        console.log(tweet, '\n\n ---------------------- \n\n')
+        tweet = `${moment().tz(timezone).format('DD/MM/YY HH:mm')}, COVID-19 no mundo. \n\nCasos confirmados: ${body.cases} \n\nRecuperados: ${body.recovered} \n\nMortes: ${body.deaths}`;
         twitterClient.tweet(tweet)
     })
 }
 
 tweetDataBr = () => {
     request.get(`${ApiUrl}/countries/brazil`, {json: true} ,(error, res, body) => {
-        tweet = `${moment().format('DD/MM/YY').toString()}, COVID-19 no Brasil. \n\nCasos confirmados: ${body.cases} \n\nRecuperados: ${body.recovered} \n\nMortes: ${body.deaths}`
-        console.log(tweet, '\n\n ---------------------- \n\n')
+        tweet = `${moment().tz(timezone).format('DD/MM/YY HH:mm')}, COVID-19 no Brasil. \n\nCasos confirmados: ${body.cases} \n\nRecuperados: ${body.recovered} \n\nMortes: ${body.deaths}`;
+        console.log(tweet);
         twitterClient.tweet(tweet)
     })
 }
 
 jobStopped = () => {
-    console.log("Cron job stopped!")
+    console.log("Cron job stopped!");
 }
 
-//Roda todo dia às 13:40:00
-// var job = new CronJob('00 */2 * * * *', tweetDataBr, jobStopped, true, 'America/Sao_Paulo');
-// job.start();
-
-var jobBrazil = new CronJob('00 9,14,19 * * * *', tweetDataWorld, jobStopped, true, 'America/Sao_Paulo');
-jobBrazil.start();
-
-var jobWorld = new CronJob('00 8,15,20 * * * *', tweetDataBr, jobStopped, true, 'America/Sao_Paulo');
+const jobWorld = new CronJob('00 00 9,16,21 * * *', tweetDataWorld, jobStopped, true, timezone);
 jobWorld.start();
 
-
+const jobBrazil = new CronJob('00 00 8,15,20 * * *', tweetDataBr, jobStopped, true, timezone);
+jobBrazil.start();
